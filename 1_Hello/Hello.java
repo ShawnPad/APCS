3d4521de7b450c15aebb109464046efca22466b7
ShawnPad <59770535+ShawnPad@users.noreply.github.com>
2020-01-11 10:34:57 -0500

    Add files via upload

---
+++ 1_Hello/Hello.java
@@ -0,0 +1,18 @@
+public class Hello{
+    private static int $money$;
+    private static int foodMoney;
+    private static int shopMoney;
+    public static void main(String[] args){
+        Math();
+        System.out.println("Hello World");
+        if($money$==19){
+            System.out.println("You have $"+$money$+"!");
+        }
+
+    }
+    public static void Math(){
+        $money$ = 9 + 10;
+        System.out.println("test");
+
+    }
+}
